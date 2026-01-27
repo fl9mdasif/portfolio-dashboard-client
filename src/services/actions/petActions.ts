04a82327759d6696f8e9e1c694295b355e403ed0
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2026-01-27 14:07:54 +0600

    fix deploy error

--- src/services/actions/petActions.ts
+++
@@ -1,9 +0,0 @@
-// "use server";
-
-// export const getAllPet = async () => {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}`, {
-//     cache: "no-store",
-//   });
-
-//   return res.json();
-// };
